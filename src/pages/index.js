2cb89ae68693dd6e47874bf01de3c034ce8ecb5d
arulprakash-gitHub <101573001+arulprakash-gitHub@users.noreply.github.com>
2024-04-14 00:39:33 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,7 +31,6 @@ const IndexPage = () => {
       <Hero
         maxWidth={'500px'}
         image={'/banner1.png'}
-        title={'Essentials for a cold winter'}
         subtitle={'Discover Autumn Winter 2021'}
         ctaText={'shop now'}
         ctaAction={goToShop}
